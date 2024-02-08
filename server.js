213b047093aab7e91a18210dc28d68a9f89c8978
Akshaya Ashok <akshayaashok1109@gmail.com>
2024-02-08 09:19:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,8 +18,8 @@ app.get('/', async(req, res) => {
 
 app.use('/articles', articleRouter);
 
-app.listen(3000,()=>
+app.listen(8080,()=>
 {
-    console.log("Server listening on port 3000");
+    console.log("Server listening on port 8080");
 }
 );
